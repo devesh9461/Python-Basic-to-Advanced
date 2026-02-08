22ba0df79f709f6652be51c9980907148556a131
DEVESH JANGID <jangiddevesh74@gmail.com>
2026-02-08 19:26:10 +0530

    Add structured learning phases to FIRST_FILE.py

---
+++ FIRST_FILE.py
@@ -0,0 +1,8 @@
+
+
+
+    Phase                	Focus	                      Key Concept
+1. The Basics	        Syntax & Logic	       Learning to "speak" the language.
+2. Core Structures	 Data & Functions	       Organizing data and writing reusable code.
+3. OOP & Modules	 Classes & Libraries       Building complex, scalable systems.
+4. Advanced	      Efficiency & Frameworks	   Writing professional, optimized code.
