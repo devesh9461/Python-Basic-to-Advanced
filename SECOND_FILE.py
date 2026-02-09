9e2546429f498f5c34b748743e171312882e0974
DEVESH JANGID <jangiddevesh74@gmail.com>
2026-02-09 18:16:37 +0530

    Create SECOND_FILE.py

---
+++ SECOND_FILE.py
@@ -0,0 +1,27 @@
+
+Which Subjects Are Python Relevant For?
+
+Computer Science
+Python can be used to learn programming
+
+Mathematics
+Python can help solve math problems and do calculations
+
+Science (Physics, Chemistry, Biology)
+Python can be used to make scientific models and undertake experiments
+
+Economics and Finance
+Python can be used to analyze data, understand trends, and do financial calculations
+
+Data Science and Analytics
+Python and its libraries can be used for big data and to find patterns
+
+Robotics and Engineering
+Python can be used to make programs to control robots or test engineering projects
+
+Art and Design
+Python can be used to create digital art, interactive projects, and automate tasks
+
+And more
+Python can be useful for many other subjects
+
