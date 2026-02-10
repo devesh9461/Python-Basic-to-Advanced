609daa303cfe30dde151c12c300ff3470675fb31
DEVESH JANGID <jangiddevesh74@gmail.com>
2026-02-10 17:34:43 +0530

    Create Keywords.py

---
+++ Keywords.py
@@ -0,0 +1,36 @@
+
+Keywords :- Keywords in Python are special reserved words that are part of the language itself. They define the rules and structure of Python programs which means you cannot use them as names for your variables, functions, classes or any other identifiers.
+
+Category	                         Keywords
+Value Keywords                     True, False, None
+Operator Keywords	                 and, or, not, is, in
+Control Flow Keywords              if, else, elif, for, while, break, continue, pass, try, except, finally, raise, assert
+Function and Class	               def, return, lambda, yield, class
+Context Management                 with, as
+Import and Module                  import, from
+Scope and Namespace                global, nonlocal
+Async Programming	                 async, await
+
+
+
+##  Keywords vs Identifiers
+
+Keywords	                                                           Identifiers
+Reserved words in Python that have a specific meaning.	             Names given to variables, functions, classes, etc.
+Cannot be used as variable names.	                                   Can be used as variable names if not a keyword.
+Examples: if, else, for, while	                                     Examples: x, number, sum, result
+Part of the Python syntax.	                                         User-defined, meaningful names in the code.
+They cannot be redefined or changed.	                               Can be defined and redefined by the programmer.
+
+
+
+## Variables vs Keywords
+
+Variables	                                                                Keywords
+Used to store data.	                                                      Reserved words with predefined meanings in Python.
+Can be created, modified, and deleted by the programmer.	                Cannot be modified or used as variable names.
+Examples: x, age, name	                                                  Examples: if, while, for
+Hold values that are manipulated in the program.	                        Used to define the structure of Python code.
+Variable names must follow naming rules but are otherwise flexible.	      Fixed by Python language and cannot be altered.
+
+
