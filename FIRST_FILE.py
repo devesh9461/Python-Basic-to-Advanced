50d6ae8b27e15f92ac5f30caf50ad8363206f86c
DEVESH JANGID <jangiddevesh74@gmail.com>
2026-02-10 16:55:37 +0530

    Delete FIRST_FILE.py

--- FIRST_FILE.py
+++
@@ -1,8 +0,0 @@
-
-
-
-    Phase                	Focus	                      Key Concept
-1. The Basics	        Syntax & Logic	       Learning to "speak" the language.
-2. Core Structures	 Data & Functions	       Organizing data and writing reusable code.
-3. OOP & Modules	 Classes & Libraries       Building complex, scalable systems.
-4. Advanced	      Efficiency & Frameworks	   Writing professional, optimized code.
